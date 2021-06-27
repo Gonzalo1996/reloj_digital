ced131b8a0edbc417afd8c37a69e6eb8469f7256
Gonzalo1996 <gonald96@gmail.com>
2021-06-26 23:42:43 -0300

    Base para arrancar.
    Creación de las carpetas, el index y el js con la fecha para ser mostrada.

---
+++ js/timer.js
@@ -0,0 +1,16 @@
+
+
+document.addEventListener("DOMContentLoaded",function () {
+    
+    setInterval(setDate,100);
+})
+
+function setDate() {
+    
+    let date = new Date(); 
+    const year = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+    const time = `${date.getHours()}:${date.getMinutes()}/${date.getSeconds()}`;
+    const pTime = document.getElementById('date');
+      
+    pTime.innerText = year + " " + time;
+}
