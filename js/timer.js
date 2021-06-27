eadb84c24a0bbc9834543a7f12290bc35f85ad1b
Gonzalo1996 <gonald96@gmail.com>
2021-06-27 02:43:34 -0300

    Estructura de alarma.
    Se creo la estructura de la pantalla de alarma con sus sass.

--- js/timer.js
+++ js/timer.js
@@ -9,7 +9,7 @@ function setDate() {
     
     let date = new Date(); 
     const year = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-    const time = `${date.getHours()}:${date.getMinutes()}/${date.getSeconds()}`;
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     const pTime = document.getElementById('date');
       
     pTime.innerText = year + " " + time;
