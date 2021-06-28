a2d4bf6bdc6845a98899c1c2bade4b7b7e68ee22
Gonzalo1996 <gonald96@gmail.com>
2021-06-27 22:04:06 -0300

    Funcionalidad agregar.
    Se creó la funcionalidad del botón agregar. Permite agregar una nueva alarma y dentro de esa misma se puede eliminar.

--- js/timer.js
+++ js/timer.js
@@ -1,16 +1,54 @@
+const $getTime = document.getElementById("timeAlarm");
+const $btnAdd = document.getElementById('newAlarm');
+const $btnEdit = document.getElementById('newAlarm');
 
-
-document.addEventListener("DOMContentLoaded",function () {
-    
-    setInterval(setDate,100);
-})
+let cont = 0;
 
 function setDate() {
     
     let date = new Date(); 
-    const year = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    const pTime = document.getElementById('date');
+    let year = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+    let time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const $pTime = document.getElementById('date');
       
-    pTime.innerText = year + " " + time;
+    $pTime.innerText = year + " " + time;
 }
+
+function addAlarm() {
+    
+    cont++
+    const $containerAlarm = document.getElementById('clock-right');
+    const $btnsAlarm = document.getElementById('alarm-btns');
+    const newAlarm = document.createElement('div');
+    const btnDelete = document.createElement('button');
+
+    newAlarm.innerHTML = 'Nueva Alarma ' + cont + "  ";
+    
+    btnDelete.innerHTML = 'x';
+    btnDelete.classList.add('button');
+
+    $containerAlarm.insertBefore(newAlarm,$btnsAlarm);
+    newAlarm.appendChild(btnDelete);
+
+    btnDelete.onclick = function () {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+        console.log('hola eliminar');
+    }
+}
+
+
+
+document.addEventListener("DOMContentLoaded",function () {
+    
+    let interval = setInterval(setDate,1000);
+    console.log($getTime)
+
+    $btnAdd.onclick = function () {
+   
+        addAlarm();
+        console.log('Agregué nueva alarma')
+    }
+
+
+    
+})
