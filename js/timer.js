32347372f0b5c5fc64394b92aa212970c0642d3f
Gonzalo1996 <gonald96@gmail.com>
2021-07-12 11:27:42 -0300

    Agregar alarma.
    Creada la función para agregar y eliminar una alarma establecida.

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,3 @@
-const $getTime = document.getElementById("timeAlarm");
 const $btnAdd = document.getElementById('newAlarm');
 const $btnEdit = document.getElementById('newAlarm');
 
@@ -14,41 +13,44 @@ function setDate() {
     $pTime.innerText = year + " " + time;
 }
 
-function addAlarm() {
-    
-    cont++
-    const $containerAlarm = document.getElementById('clock-right');
-    const $btnsAlarm = document.getElementById('alarm-btns');
-    const newAlarm = document.createElement('div');
+function btnDelete(newAlarm) {
     const btnDelete = document.createElement('button');
-
-    newAlarm.innerHTML = 'Nueva Alarma ' + cont + "  ";
     
+    newAlarm.appendChild(btnDelete);
     btnDelete.innerHTML = 'x';
     btnDelete.classList.add('button');
-
-    $containerAlarm.insertBefore(newAlarm,$btnsAlarm);
-    newAlarm.appendChild(btnDelete);
-
     btnDelete.onclick = function () {
         this.parentNode.parentNode.removeChild(this.parentNode);
-        console.log('hola eliminar');
     }
 }
 
+function addAlarm() {
+    
+    let $getTime = document.getElementById("timeAlarm").value;
+    const $containerAlarm = document.getElementById('clock-right');
+    const $alarm = document.getElementById('alarm-btns');
+    const newAlarm = document.createElement('div');
+  
+    newAlarm.innerHTML = `<p id='prueba' style='display: inline-block'>${$getTime}</p>`;
+    $getTime = document.getElementById("timeAlarm").value ="00:00:00";
+    $containerAlarm.insertBefore(newAlarm,$alarm);
 
+    btnDelete(newAlarm);
+    
+   
+    //document.getElementById('prueba').innerText = $getTime;
+    //$containerAlarm.insertBefore(btnDelete,$btnsAlarm);
+   // console.log("Existo? : ",$getTime);
+}
 
 document.addEventListener("DOMContentLoaded",function () {
     
-    let interval = setInterval(setDate,1000);
-    console.log($getTime)
+    let interval = setInterval(setDate);
+
 
     $btnAdd.onclick = function () {
    
         addAlarm();
         console.log('Agregué nueva alarma')
     }
-
-
-    
 })
